a851662b7f6d8d4831a336a713388ced8fe73312
David Rapoport <davirapo@gmail.com>
2023-07-09 22:35:06 -0300

    Agregando conexion con fronted y CORS

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require("dotenv").config()
 const { Sequelize, Model, DataTypes } = require("sequelize")
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
 const avionRouter = require("./routes/aviones")
 const aeropuertoRouter = require("./routes/aeropuertos")
 const rutasRouter = require("./routes/rutas")
+app.use(cors())
 
 app.use("/api", aeropuertoRouter)
 app.use("/api", avionRouter)
